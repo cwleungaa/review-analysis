761e661161a55688ab76c8eb02d7ba9aa5499e89
cwleungaa <cwleungaa@gmail.com>
2019-03-01 03:56:48 +0800

    filler

--- read.py
+++ read.py
@@ -16,3 +16,15 @@ average = total / len(data)
 print ('留言的平均長度是 ', average, '句')
 
 
+data_L = []
+data_S = []
+
+for e in data:
+	if len(e) <= 100:
+		data_S.append(e)
+	elif len(e) > 100:
+		data_L.append(e)
+
+print('長度少於100字的留言有 ', len(data_S),'筆')
+
+print('長度大於100字的留言有 ', len(data_L),'筆')
